f28cdde2de6e26dc9e74c390e673fb033308cc31
Tanjil <tanjilkh@gmail.com>
2024-04-30 16:33:42 +0100

    sign up form

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,11 @@ const signup = async(req, res) => {
 			res.status(500).json({message: "User already exists."})
 		} else {
 			const user = await User.create(req.body)
-			res.status(200).json(user)
+			console.log(user)
+			console.log(user.username)
+			req.session.loggedIn = true
+			req.session.username = user.username
+			res.redirect('/xfiles')
 		}
 	} catch (error) {
 		res.status(500).json({message: error.message})
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,13 @@ const router = express.Router()
 // Rotas para autenticação
 router.post('/signup', authController.signup)
 router.post('/signin', authController.signin)
+
+// Rotas para renderizar as paginas
 router.get('/signin', (req, res) => {
 	res.render('signin')
+})
+
+router.get('/signup', (req, res) => {
+	res.render('signup')
 })
 module.exports = router

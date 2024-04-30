6b68d56ef70b67304993342425fcb382c787d942
Tanjil <tanjilkh@gmail.com>
2024-04-30 11:58:09 +0100

    rota signin e adicionar comentarios

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,12 @@
 const User = require("../models/userModel")
 const UserController = require("../controllers/userController")
+
+// Função para registar uma conta de utilizador
 const signup = async(req, res) => {
 	try{
+		// Verifica se já existe um utilizador com esse username
+		// Se ainda não existir uma conta com esse username
+		// Cria conta
 		const user_exists = await User.find({username: req.body.username})
 		if(user_exists.length){
 			res.status(500).json({message: "User already exists."})
@@ -14,4 +19,8 @@ const signup = async(req, res) => {
 	}
 }
 
-module.exports = {signup}
+// Função para autenticar numa conta de utilizador
+const signin = async(req, res) => {
+	
+}
+module.exports = {signup, signin}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,6 +4,8 @@ const authController = require("../controllers/authController")
 
 const router = express.Router()
 
+// Rotas para autenticação
 router.post('/signup', authController.signup)
+router.post('/signin', authController.signin)
 
 module.exports = router

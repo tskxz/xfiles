3dd08de04fe50c53ded8f7da8babd04c1f4682d3
Tanjil <tanjilkh@gmail.com>
2024-04-30 11:51:09 +0100

    auth signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,17 @@
 const User = require("../models/userModel")
-
+const UserController = require("../controllers/userController")
 const signup = async(req, res) => {
 	try{
-		const user = await User.create(req.body)
-		res.status(200).json(user)
-		console.log(user)
+		const user_exists = await User.find({username: req.body.username})
+		if(user_exists.length){
+			res.status(500).json({message: "User already exists."})
+		} else {
+			const user = await User.create(req.body)
+			res.status(200).json(user)
+		}
 	} catch (error) {
 		res.status(500).json({message: error.message})
 	}
 }
-
 
 module.exports = {signup}

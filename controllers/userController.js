62150f3cec735a358219818af67bf96293ab46d1
Tanjil <tanjilkh@gmail.com>
2024-05-09 15:05:56 +0100

    add get user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,8 @@ const getUsers = async (req, res) => {
 
 // Funcao para criar utilizador
 const createUser = async(req, res ) => {
+
+	// Tem que se melhorar nesta parte, mandar um aviso se o username já existir.
 	try {
 		const user = await User.create(req.body)
 		res.status(200).json(user)
@@ -20,5 +22,15 @@ const createUser = async(req, res ) => {
 	}
 }
 
+// Funcao para buscar um utilizador
+const getUser = async(req, res) => {
+	try{
+		const user = await User.find({_id: req.params.id})
+		res.status(200).json(user)
+	} catch(error){
+		res.status(500).json({message: error.message})
+	}
+}
 
-module.exports = {getUsers, createUser}
+
+module.exports = {getUsers, createUser, getUser}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const User = require("../models/userModel")
 const UserController = require("../controllers/userController")
-const {getUsers, createUser} = require("../controllers/userController")
+const {getUsers, createUser, getUser} = require("../controllers/userController")
 
 const router = express.Router()
 
 router.get('/', getUsers)
 router.post('/', createUser)
+router.get('/:id', getUser)
 
 module.exports = router
